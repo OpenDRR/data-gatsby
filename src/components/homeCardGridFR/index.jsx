97b843b643e90be3db7705d63e1ada25b975859a
Michael <michaelchristianson93@gmail.com>
2021-04-08 15:14:49 -0700

    /fr/ paths updated in config files. provincial and territorial pages updated to new paths

--- src/components/homeCardGridFR/index.jsx
+++ src/components/homeCardGridFR/index.jsx
@@ -1,23 +1,8 @@
 import React from "react";
-import { 
-  Link
-  //  useStaticQuery,
-  //   graphql 
-  } from "gatsby";
+import { Link } from "gatsby";
 
-const HomeCardGrid = () => {
-//   const data = useStaticQuery(graphql`
-//     query {
-//       markdownRemark {
-//         frontmatter {
-//           title
-//           slug
-//           date
-//         }
-//         id
-//       }
-//     }
-//   `);
+const HomeCardGridFR = () => {
+
 
   return (
     <>
@@ -28,14 +13,8 @@ const HomeCardGrid = () => {
       <Link to="/fr/Provinces-et-Territoires">Provinces et Territoires </Link>
     </div>
    </div>
-    {/* <div className="col-md-12 LNU">
-      <h2>Latest News & Updates</h2>
-      <Link to={data.markdownRemark.frontmatter.slug}>
-        <p>{data.markdownRemark.frontmatter.title}</p>
-      </Link>
-    </div> */}
     </>
   );
 };
 
-export default HomeCardGrid;
+export default HomeCardGridFR;
